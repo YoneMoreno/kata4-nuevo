6529115361a31444c7f22e2649a41ad7da361d7f
usuario <usuario@10.22.143.48>
2016-11-03 11:47:20 +0100

    Kata 4 v3 creamos mail, MailListReader, MailHistogramBuilder y modificamos main. Aprendemos que una variable private final no hay que inciializarla porque entonces el constructor recibe la ristra pero no la asocia con el contenido, porque por defecto éste está  a nulo y no recibe.

--- src/kata4/Kata4.java
+++ src/kata4/Kata4.java
@@ -1,26 +1,19 @@
 package kata4;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class Kata4 {
 
-    public static void main(String[] args) {
-        File file = new File("C:\\Program Files\\NetBeans 7.3.1");
-        print(file.listFiles(), "");
-
-
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        String fileName = "C:\\Users\\usuario\\Documents\\NetBeansProjects\\Kata4\\emailsfilev1.txt";
+        ArrayList<String> mailList = MailListReader.read(fileName);
+        Histogram<String> histogram = MailHistogramBuilder.build(mailList);
+        HistogramDisplay histoDisplay = new HistogramDisplay(histogram);
+        histoDisplay.execute();
     }
 
-    private static void print(File[] files, String indent) {
-        if (files == null) {
-            return;
-        }
-        for (File file : files) {
-            System.out.println(indent + (file.isDirectory() ? "+" : "-") + file.getName());
-            if (!file.isDirectory() || file.isHidden()) {
-                continue;
-            }
-            print(file.listFiles(), indent+" ");
-        }
-    }
+
 }
